7bc29bd21ded6b9d93578ddf59f8d426b68dc18f
juan.mendez <juan.mendez@vasster.com>
2018-11-27 15:33:50 -0600

    first commit

---
+++ table.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="./sorttable.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <table class="sortable">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>nombre</th>
+                <th>correo</th>
+            </tr>            
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>juan</td>
+                <td>juan@gmail.com</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>carlos</td>
+                <td>carlos@gmail.com</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>mariana</td>
+                <td>mariana@gmail.com</td>
+            </tr>
+        </tbody>
+    </table>  
+</body>
+</html>
